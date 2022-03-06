9d9fa80c8cd57125e4d1ae644d5b43e9445b4d48
pradeep <dashwar.pradeep@gmail.com>
2022-03-06 05:58:40 +0530

    initial commit

---
+++ src/main/java/io/javabrains/springbootstarter/topic/TopicRepository.java
@@ -0,0 +1,83 @@
+package io.javabrains.springbootstarter.topic;
+
+import java.util.Optional;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface TopicRepository extends CrudRepository<Topic,String>{
+
+//	@Override
+//	public <S extends Topic> S save(S entity) {
+//		// TODO Auto-generated method stub
+//		return null;
+//	}
+//
+//	@Override
+//	public <S extends Topic> Iterable<S> saveAll(Iterable<S> entities) {
+//		// TODO Auto-generated method stub
+//		return null;
+//	}
+//
+//	@Override
+//	public Optional<Topic> findById(String id) {
+//		// TODO Auto-generated method stub
+//		return null;
+//	}
+//
+//	@Override
+//	public boolean existsById(String id) {
+//		// TODO Auto-generated method stub
+//		return false;
+//	}
+//
+//	@Override
+//	public Iterable<Topic> findAll() {
+//		// TODO Auto-generated method stub
+//		return null;
+//	}
+//
+//	@Override
+//	public Iterable<Topic> findAllById(Iterable<String> ids) {
+//		// TODO Auto-generated method stub
+//		return null;
+//	}
+//
+//	@Override
+//	public long count() {
+//		// TODO Auto-generated method stub
+//		return 0;
+//	}
+//
+//	@Override
+//	public void deleteById(String id) {
+//		// TODO Auto-generated method stub
+//		
+//	}
+//
+//	@Override
+//	public void delete(Topic entity) {
+//		// TODO Auto-generated method stub
+//		
+//	}
+//
+//	@Override
+//	public void deleteAllById(Iterable<? extends String> ids) {
+//		// TODO Auto-generated method stub
+//		
+//	}
+//
+//	@Override
+//	public void deleteAll(Iterable<? extends Topic> entities) {
+//		// TODO Auto-generated method stub
+//		
+//	}
+//
+//	@Override
+//	public void deleteAll() {
+//		// TODO Auto-generated method stub
+//		
+//	}
+	
+
+
+}
